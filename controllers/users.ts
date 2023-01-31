bab016c858ef8fdb7911ca2ed1c197e615562e13
Bryan31542 <00063820@uca.edu.sv>
2023-01-30 20:37:24 -0600

    feat: get all and get one user :sparkles:

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,22 +1,30 @@
 import { Request, Response } from 'express';
+import User from '../models/user';
 
-export const getUsers = (req: Request, res: Response) => {
+export const getUsers = async (req: Request, res: Response) => {
+
+    const users = await User.findAll();
 
     res.json({
-        ok: true,
-        msg: 'getUsers'
+        users
     });
 }
 
-export const getUser = (req: Request, res: Response) => {
+export const getUser = async (req: Request, res: Response) => {
 
     const { id } = req.params;
 
-    res.json({
-        ok: true,
-        msg: 'getUser',
-        id
-    });
+    const user = await User.findByPk(id);
+
+    if (!user) {
+        return res.status(404).json({
+            msg: `User with id ${id} not found`
+        });
+    }
+
+    res.json(
+        user
+    );
 }
 
 export const postUser = (req: Request, res: Response) => {
